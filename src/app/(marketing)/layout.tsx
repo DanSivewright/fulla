a8ae8b48e7e788664cadd77520adc61858e897f8
Dan Sivewright <dan@wixels.com>
2024-07-27 13:30:51 +0200

    root layout for marketing

---
+++ src/app/(marketing)/layout.tsx
@@ -0,0 +1,9 @@
+export default function RootMarketingLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        <main>{children}</main>
+      </body>
+    </html>
+  )
+}
