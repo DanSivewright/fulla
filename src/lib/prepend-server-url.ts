c5b6179b9e1b5257435b7907aa8f8b57ad36ad8f
Dan Sivewright <dan@wixels.com>
2024-07-30 15:18:55 +0200

    use VERCEL_URL not public image url

--- src/lib/prepend-server-url.ts
+++ src/lib/prepend-server-url.ts
@@ -1,5 +1,3 @@
 export const prependServerUrl = (url: string) => {
-  return `${
-    process.env.NODE_ENV !== 'development' ? process.env.NEXT_PUBLIC_SERVER_URL + '/' : ''
-  }${url}`
+  return `${process.env.NODE_ENV !== 'development' ? process.env.VERCEL_URL + '/' : ''}${url}`
 }
