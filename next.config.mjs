4df2fdee296cc5554f2900943c58b652f30ee2c9
Dan Sivewright <dan@wixels.com>
2024-07-30 15:49:37 +0200

    add blobl remote url to config for testing

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,15 @@ import { withPayload } from '@payloadcms/next/withPayload'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'fulla.public.blob.vercel-storage.com',
+        port: '',
+      },
+    ],
+  },
 }
 
 export default withPayload(nextConfig)
--- src/lib/prepend-server-url.ts
+++ src/lib/prepend-server-url.ts
@@ -1,3 +1,4 @@
 export const prependServerUrl = (url: string) => {
-  return `${process.env.NEXT_PUBLIC_APP_URL}${url}`
+  return url
+  // return `${process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_APP_URL : ''}${url}`
 }
