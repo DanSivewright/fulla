8146b5330934706d395d4923f0b3f7c92bdd3e4d
Dan Sivewright <dan@wixels.com>
2024-08-03 11:50:36 +0200

    redirect from root to marketing

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,75 +1,7 @@
-'use client'
-
-import { useState } from 'react'
+import { redirect } from 'next/navigation'
 
 type Props = {}
-export const Test = ({}) => {
-  const [previousSearches, setPreviousSearches] = useState([])
-
-  const [search, setSearch] = useState('')
-  const [city, setCity] = useState(null)
-
-  const mockWeatherData = {
-    'New York': {
-      temperature: '22˚C',
-      humidity: '56%',
-      windSpeed: '15 km/h',
-    },
-    'Los Angeles': {
-      temperature: '27˚C',
-      humidity: '45%',
-      windSpeed: '10 km/h',
-    },
-  }
-
-  const simulateWeatherApiCall = () => {
-    if (mockWeatherData[search]) {
-      setCity(search)
-      setSearch('')
-    } else {
-      setCity(null)
-    }
-    setPreviousSearches((prev) => [...prev, search])
-  }
-
-  return (
-    <div>
-      <input
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-        type="text"
-        id="citySearch"
-        placeholder="Search for a city..."
-      />
-      <button
-        onClick={() => {
-          simulateWeatherApiCall()
-        }}
-        id="searchButton"
-      >
-        Search
-      </button>
-      <div id="weatherData">
-        <div>Temperature: </div>
-        <div>Humidity: </div>
-        <div>Wind Speed: </div>
-        {previousSearches?.length && !city ? <div>City not found.</div> : null}
-      </div>
-      <div id="previousSearches">
-        {previousSearches?.length ? (
-          <>
-            <p>Previous Searches:</p>
-            <ul>
-              {previousSearches.map((search, index) => (
-                <li key={search + index}>{search}</li>
-              ))}
-            </ul>
-          </>
-        ) : (
-          'No previous searches'
-        )}
-      </div>
-    </div>
-  )
+const Root: React.FC<Props> = ({}) => {
+  return redirect('/marketing')
 }
-export default Test
+export default Root
