8950e28ab1113419ea494941246f31a8ed5a8341
Dan Sivewright <dan@wixels.com>
2024-08-05 14:33:11 +0200

    Update prepend-server-url.ts

--- src/lib/prepend-server-url.ts
+++ src/lib/prepend-server-url.ts
@@ -1,3 +1,7 @@
 export const prependServerUrl = (url: string) => {
-  return `${process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_APP_URL : ''}${url}`
+  return `${
+    process.env.NODE_ENV === "production"
+      ? process.env.NEXT_PUBLIC_SERVER_URL
+      : ""
+  }${url}`
 }
