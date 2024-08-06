11c685783f56bd0a5baac7639c65ad981ac31643
Dan Sivewright <dan@wixels.com>
2024-08-06 10:02:34 +0200

    force dynamic and revalidate for collections page

--- src/app/(site)/collections/page.tsx
+++ src/app/(site)/collections/page.tsx
@@ -1,12 +1,10 @@
-import { headers } from "next/headers"
-import config from "@payload-config"
-import configPromise from "@payload-config"
-import { getPayloadHMR } from "@payloadcms/next/utilities"
-
 import { createPage } from "@/lib/create-page"
 import { getCachedCollection } from "@/lib/get-cached-collection"
 import { Section } from "@/components/section"
 import { Title } from "@/components/title"
+
+export const dynamic = "force-dynamic"
+export const revalidate = 2400
 
 const { Page } = createPage({
   loader: async () => {
