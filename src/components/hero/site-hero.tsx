56715ced446d37d45d0ea7c44307e001bf20ce5a
Dan Sivewright <dan@wixels.com>
2024-08-08 14:59:34 +0200

    default state for hidden

--- src/components/hero/site-hero.tsx
+++ src/components/hero/site-hero.tsx
@@ -1,27 +1,30 @@
-'use client'
+"use client"
 
-import { useEffect, useState } from 'react'
-import Link from 'next/link'
-import { useInView } from '@/hooks/use-in-view'
-import { Title } from '../title'
-import { Paragraph } from '../paragraph'
-import { buttonVariants } from '../ui/button'
-import { useLocalStorage } from '@/hooks/use-local-storage'
+import { useEffect, useState } from "react"
+import Link from "next/link"
+
+import { useInView } from "@/hooks/use-in-view"
+import { useLocalStorage } from "@/hooks/use-local-storage"
+
+import { Paragraph } from "../paragraph"
+import { Title } from "../title"
+import { buttonVariants } from "../ui/button"
 
 type Props = {}
 export const SiteHero: React.FC<Props> = ({}) => {
-  const [hasTriggeredHeroScroller, setHasTriggeredHeroScroller] = useState(false)
+  const [hasTriggeredHeroScroller, setHasTriggeredHeroScroller] =
+    useState(false)
 
   const [hidden, setHidden] = useLocalStorage({
-    key: 'nav-hidden',
+    key: "nav-hidden",
     defaultValue: false,
   })
   const [background, setBackground] = useLocalStorage({
-    key: 'nav-background',
+    key: "nav-background",
     defaultValue: true,
   })
   const [blur, setBlur] = useLocalStorage({
-    key: 'nav-blur',
+    key: "nav-blur",
     defaultValue: false,
   })
 
@@ -30,7 +33,13 @@ export const SiteHero: React.FC<Props> = ({}) => {
     initialInView: true,
     onChange(inView) {
       if (inView) {
-        console.log('have i triggered scroll state::: ', hasTriggeredHeroScroller)
+        if (hidden) {
+          setHidden(true)
+        }
+        console.log(
+          "have i triggered scroll state::: ",
+          hasTriggeredHeroScroller
+        )
         if (hasTriggeredHeroScroller) {
           setBlur(true)
           //   dispatch({ type: 'field', payload: true, field: 'blur' })
@@ -75,21 +84,34 @@ export const SiteHero: React.FC<Props> = ({}) => {
 
   return (
     <div
-      style={{ width: '100vw' }}
+      style={{ width: "100vw" }}
       className="card relative mt-[-3.7rem] h-[480px] w-screen bg-stone-900"
     >
-      <div ref={heroRef} className="absolute inset-x-0 bottom-[4rem] h-1 w-full"></div>
+      <div
+        ref={heroRef}
+        className="absolute inset-x-0 bottom-[4rem] h-1 w-full"
+      ></div>
       <div className="card-content gutter flex h-full w-full items-center justify-normal">
         <div className="relative mx-auto flex w-full max-w-3xl flex-col items-center justify-center text-center">
-          <div ref={ref} className="absolute inset-x-0 top-[-2rem] h-2 w-full"></div>
-          <Title className="font-mono text-balance font-bold text-white" level={2} showAs={3}>
+          <div
+            ref={ref}
+            className="absolute inset-x-0 top-[-2rem] h-2 w-full"
+          ></div>
+          <Title
+            className="font-mono text-balance font-bold text-white"
+            level={2}
+            showAs={3}
+          >
             Work Smarter, Not Harder – Go Pro!
           </Title>
 
           <Paragraph className="text-white text-pretty">
             Stand Out, Sign Up. Pro Members Get Noticed First!
           </Paragraph>
-          <Link href={'/profile'} className={buttonVariants({ className: 'mt-4' })}>
+          <Link
+            href={"/profile"}
+            className={buttonVariants({ className: "mt-4" })}
+          >
             Go Pro!
           </Link>
         </div>
